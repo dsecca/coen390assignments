269863317e6113380d414eb6dbc419c29c458bbb
dsecca <davsecc1995@gmail.com>
2017-02-06 08:41:29 -0500

    Added necessary files and added the SharedPreferenceHelper class

---
+++ ProfileAssignment/app/src/main/java/test/profileassignment/SharedPreferenceHelper.java
@@ -0,0 +1,24 @@
+package test.profileassignment;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by David on 2017-02-06.
+ */
+/*CONTROLLER FOR SHARED PREFERENCES*/
+
+public class SharedPreferenceHelper{
+
+    private SharedPreferences sharedPreferences;
+    public SharedPreferenceHelper(Context context){
+
+        sharedPreferences = context.getSharedPreferences("ProfilePreference",Context.MODE_PRIVATE);
+    }
+
+    public void saveProfileName(String name){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("profileName",name);
+        editor.commit();
+    }
+}
