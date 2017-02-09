747abeabcc7fcbc668e7f4c450aeb0e29e3633a6
dsecca <davsecc1995@gmail.com>
2017-02-08 23:19:10 -0500

    App edits entries and displays profile info after pushing Profile button

--- ProfileAssignment/app/src/main/java/test/profileassignment/ProfileActivity.java
+++ ProfileAssignment/app/src/main/java/test/profileassignment/ProfileActivity.java
@@ -1,5 +1,6 @@
 package test.profileassignment;
 
+import android.content.ClipData;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -17,6 +18,7 @@ public class ProfileActivity extends AppCompatActivity {
     protected EditText profileName = null;
     protected EditText profileAge = null;
     protected EditText studentID = null;
+    protected Button profileSaveButton = null;
     protected static Profile profile = new Profile();
     private SharedPreferenceHelper sharedPreferenceHelper = null;
     @Override
@@ -34,7 +36,17 @@ public class ProfileActivity extends AppCompatActivity {
         profileAge = (EditText) findViewById(R.id.profileAge);
         studentID = (EditText) findViewById(R.id.studentID);
 
-        final Button profileSaveButton = (Button) findViewById(R.id.profileSaveButton);
+        //Initially make fields unedittable
+        profileName.setFocusable(false);
+        profileAge.setFocusable(false);
+        studentID.setFocusable(false);
+
+        //Initialize save button but set it as invisible till edit button
+        //is pressed
+        profileSaveButton = (Button) findViewById(R.id.profileSaveButton);
+        profileSaveButton.setVisibility(View.INVISIBLE);
+        /*profileSaveButton = (Button) findViewById(R.id.profileSaveButton);
+
 
         profileSaveButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -43,17 +55,9 @@ public class ProfileActivity extends AppCompatActivity {
                 String age = profileAge.getText().toString();
                 String id = studentID.getText().toString();
 
-                /*System.out.println(name);
-                System.out.println(age);
-                System.out.println(id);*/
-
                 profile.saveName(name);
                 profile.saveAge(age);
                 profile.saveID(id);
-
-                /*System.out.println(profile.getName());
-                System.out.println(profile.getAge());
-                System.out.println(profile.getID());*/
 
 
                 //here we'll pass in the newly saved object to the shared preferences
@@ -62,14 +66,14 @@ public class ProfileActivity extends AppCompatActivity {
                 /*Toast toast = Toast.makeText(getApplicationContext(), "saved" , Toast.LENGTH_LONG);
                 toast.show();*/
 
-            }
-        });
+            //}
+       // });*/
 
-        if((profile.getName() != null) && (profile.getAge() != null) && (profile.getID() != null)){
+        /*if((profile.getName() != null) && (profile.getAge() != null) && (profile.getID() != null)){
             profileName.setText(profile.getName());
             profileAge.setText(profile.getAge());
             studentID.setText(profile.getID());
-        }
+        }*/
     }
 
     //Create the menu on the toolbar
@@ -90,9 +94,55 @@ public class ProfileActivity extends AppCompatActivity {
                 return true;
             case R.id.action_setting:
                 //call the editing button function
+                editFields();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    protected void onStart(){
+        super.onStart();
+        Profile profile = sharedPreferenceHelper.getProfile();
+
+        profileName.setText(profile.getName());
+        profileAge.setText(profile.getAge());
+        studentID.setText(profile.getID());
+    }
+
+    protected void editFields(){
+        //All fields to be editable
+        profileName.setFocusableInTouchMode(true);
+        profileAge.setFocusableInTouchMode(true);
+        studentID.setFocusableInTouchMode(true);
+        profileSaveButton.setVisibility(View.VISIBLE);
+
+        profileSaveButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                String name = profileName.getText().toString();
+                String age = profileAge.getText().toString();
+                String id = studentID.getText().toString();
+
+                profile.saveName(name);
+                profile.saveAge(age);
+                profile.saveID(id);
+
+
+                //here we'll pass in the newly saved object to the shared preferences
+                sharedPreferenceHelper.saveProfile(profile);
+
+                /*Toast toast = Toast.makeText(getApplicationContext(), "saved" , Toast.LENGTH_LONG);
+                toast.show();*/
+
+                //Make fields unedittable again
+                profileName.setFocusable(false);
+                profileAge.setFocusable(false);
+                studentID.setFocusable(false);
+                profileSaveButton.setVisibility(View.INVISIBLE);
+
+
+            }
+        });
     }
 }
